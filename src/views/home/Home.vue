c5382f7ba164683e6844011cad6f35851c7ea2a8
zhangliang <zhangliang@acw.sc.cn>
2021-08-04 09:09:09 +0800

    第二次提交

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,19 +5,70 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommend"/>
+    <feature-view/>
+    <tab-control :titles="['流行', '新款', '精选']" class="tab-control"/>
+    <ul>
+      <li>李彪1</li>
+      <li>李彪2</li>
+      <li>李彪3</li>
+      <li>李彪4</li>
+      <li>李彪5</li>
+      <li>李彪6</li>
+      <li>李彪7</li>
+      <li>李彪8</li>
+      <li>李彪9</li>
+      <li>李彪10</li>
+      <li>李彪11</li>
+      <li>李彪12</li>
+      <li>李彪13</li>
+      <li>李彪14</li>
+      <li>李彪15</li>
+      <li>李彪16</li>
+      <li>李彪17</li>
+      <li>李彪18</li>
+      <li>李彪19</li>
+      <li>李彪20</li>
+      <li>李彪21</li>
+            <li>李彪11</li>
+      <li>李彪12</li>
+      <li>李彪13</li>
+      <li>李彪14</li>
+      <li>李彪15</li>
+      <li>李彪16</li>
+      <li>李彪17</li>
+      <li>李彪18</li>
+      <li>李彪19</li>
+      <li>李彪20</li>
+      <li>李彪21</li>
+            <li>李彪11</li>
+      <li>李彪12</li>
+      <li>李彪13</li>
+      <li>李彪14</li>
+      <li>李彪15</li>
+      <li>李彪16</li>
+      <li>李彪17</li>
+      <li>李彪18</li>
+      <li>李彪19</li>
+      <li>李彪20</li>
+      <li>李彪21</li>
+    </ul>
   </div>
 
 </template>
 
 <script>
     import NavBar from "components/common/navbar/NavBar";
+
     import HomeSwiper from "./childComps/HomeSwiper";
     import RecommendView from "./childComps/RecommendView";
+    import FeatureView from "./childComps/FeatureView";
+    import TabControl from "components/content/tabControl/TabControl";
+
     import {getHomeMultidata} from "network/home";
 
     export default {
         name: "Home",
-        components: {HomeSwiper, NavBar, RecommendView},
+        components: {HomeSwiper, NavBar, RecommendView, FeatureView, TabControl},
         data() {
             return {
                 banners: [],
@@ -37,6 +88,17 @@
   .home-nav {
     background-color: var(--color-tint);
     color: #f6f6f6;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
-
+  #home {
+    padding-top: 44px;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+  }
 </style>
